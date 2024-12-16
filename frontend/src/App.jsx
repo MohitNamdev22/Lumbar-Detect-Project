c69c8f51ac784ff677f60878c8fbc807ce713843
Mohit Namdev <namdevmohit0@gmail.com>
2024-12-16 18:20:34 +0530

    fixed the color of select file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -87,7 +87,7 @@ export default function App() {
               <p className="mt-4 text-lg text-gray-600">Drag and drop your MRI image here, or click to select a file</p>
               <input {...getInputProps()} className="hidden" accept=".jpg, .png, .dcm"/>
               <button
-              className="mt-6 inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              className="mt-6 inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               onClick={(event) => {
                 event.stopPropagation(); // Prevent opening the file picker
                 document.querySelector('input[type="file"]').click();
